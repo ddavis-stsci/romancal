3b66260e84725021d7b730435e119261cca8ecc4
Dave Davis <ddavis@stsci.edu>
2025-05-09 10:46:48 -0400

    rcal-1038 Clean up unit tests

--- romancal/source_catalog/tests/test_source_catalog.py
+++ romancal/source_catalog/tests/test_source_catalog.py
@@ -91,7 +91,6 @@ def image_model():
     return model
 
 
-@pytest.mark.stpsf
 def test_forced_catalog(image_model, tmp_path):
     os.chdir(tmp_path)
     step = SourceCatalogStep()
@@ -123,7 +122,6 @@ def test_forced_catalog(image_model, tmp_path):
     assert has_forced_fields
 
 
-@pytest.mark.stpsf
 @pytest.mark.parametrize(
     "snr_threshold, npixels, nsources, save_results",
     (
@@ -168,7 +166,6 @@ def test_l2_source_catalog(
         assert np.max(cat["y_centroid"]) < 100.0
 
 
-@pytest.mark.stpsf
 @pytest.mark.parametrize(
     "snr_threshold, npixels, nsources, save_results",
     (
@@ -181,6 +178,7 @@ def test_l2_source_catalog(
         (50, 10, 0, False),
     ),
 )
+@pytest.mark.skip(reason="mosaic model does not have correct CRDS info")
 def test_l3_source_catalog(
     mosaic_model, snr_threshold, npixels, nsources, save_results, tmp_path
 ):
@@ -215,7 +213,7 @@ def test_l3_source_catalog(
         assert np.max(cat["y_centroid"]) < 100.0
 
 
-@pytest.mark.stpsf
+@pytest.mark.skip(reason="mosaic model does not have correct CRDS info")
 def test_background(mosaic_model, tmp_path):
     """
     Test background fallback when Background2D fails.
@@ -236,7 +234,6 @@ def test_background(mosaic_model, tmp_path):
     assert isinstance(cat, Table)
 
 
-@pytest.mark.stpsf
 def test_l2_input_model_unchanged(image_model, tmp_path):
     """
     Test that the input model data and error arrays are unchanged after
@@ -261,7 +258,7 @@ def test_l2_input_model_unchanged(image_model, tmp_path):
     assert_equal(original_err, image_model.err)
 
 
-@pytest.mark.stpsf
+@pytest.mark.skip(reason="mosaic model does not have correct CRDS info")
 def test_l3_input_model_unchanged(mosaic_model, tmp_path):
     """
     Test that the input model data and error arrays are unchanged after
@@ -286,7 +283,6 @@ def test_l3_input_model_unchanged(mosaic_model, tmp_path):
     assert_equal(original_err, mosaic_model.err)
 
 
-@pytest.mark.stpsf
 def test_inputs(mosaic_model):
     data = np.ones((3, 3), dtype=int)
     data[1, 1] = 1
@@ -297,7 +293,6 @@ def test_inputs(mosaic_model):
         RomanSourceCatalog(np.ones((3, 3)), segm, cdata, kernel_fwhm, fit_psf=True)
 
 
-@pytest.mark.stpsf
 def test_psf_photometry(tmp_path, image_model):
     """
     Test PSF photometry.
@@ -330,7 +325,6 @@ def test_psf_photometry(tmp_path, image_model):
             assert not np.any(np.isnan(cat[colname]))  # and contains no nans
 
 
-@pytest.mark.stpsf
 @pytest.mark.parametrize("fit_psf", [True, False])
 def test_do_psf_photometry_column_names(tmp_path, image_model, fit_psf):
     """
@@ -362,7 +356,6 @@ def test_do_psf_photometry_column_names(tmp_path, image_model, fit_psf):
         assert len(psf_colnames) == 0
 
 
-@pytest.mark.stpsf
 @pytest.mark.parametrize(
     "snr_threshold, npixels, nsources, save_results, return_updated_model, expected_result, expected_outputs",
     (
@@ -468,7 +461,6 @@ def test_l2_source_catalog_keywords(
             assert isinstance(rdm.open(filepath), expected_outputs.get(suffix))
 
 
-@pytest.mark.stpsf
 @pytest.mark.parametrize(
     "snr_threshold, npixels, nsources, save_results, return_updated_model, expected_result, expected_outputs",
     (
@@ -523,6 +515,7 @@ def test_l2_source_catalog_keywords(
         ),
     ),
 )
+@pytest.mark.skip(reason="mosaic model does not have correct CRDS info")
 def test_l3_source_catalog_keywords(
     mosaic_model,
     snr_threshold,
@@ -574,7 +567,6 @@ def test_l3_source_catalog_keywords(
             assert isinstance(rdm.open(filepath), expected_outputs.get(suffix))
 
 
-@pytest.mark.stpsf
 @pytest.mark.parametrize(
     "return_updated_model, expected_result",
     (
